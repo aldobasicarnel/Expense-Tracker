021326acb0544f3dcc9506a2eb3f4d872b4e1ff9
aldobasicarnel <arnelaldobasic8@gmail.com>
2022-09-21 23:24:05 +0200

    'Completing heroku setup'

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.use("/api/transactions", transactions);
 
 const PORT = process.env.PORT || 7006;
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = PORT;
+}
+
 app.listen(PORT, function (req, res) {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
